a6f3b051a65465992fb3169bb3e05c1e1cc8e0ac
cycki <spamelami@gmail.com>
2014-04-13 21:15:34 +0200

    Added Scheduler Statisctics
    Schedulery też mają jakieśtam staty, te same co w Admission Control

--- src/Scheudler/AbstractScheduler.cc
+++ src/Scheudler/AbstractScheduler.cc
@@ -1,35 +1,62 @@
 #include "AbstractScheduler.h"
 
 void AbstractScheduler::initialize() {
+    //statistics
+    emitBandwidth = new cMessage("emitBandwith");
+    delaySignal = registerSignal("delay");
+    rejectedSignal = registerSignal("rejected");
+    intervalSignal = registerSignal("interval");
+    bandwidthSignal = registerSignal("bandwidth");
+    timeRequestHistogram.setRange(0, 1700);
+    timeRequestHistogram.setNumCells(1000);
+    prevTime = 0;
+    bandwidth = 0;
+    rejectedCount = acceptedCount = 0;
+
     processEvent = new cMessage("processEvent");
+    processDelay = par("processDelay");
+
+    scheduleAt(simTime() + 1, emitBandwidth);
 }
 
 void AbstractScheduler::handleMessage(cMessage* msg) {
-    simtime_t processDelay = par("processDelay");
-
-    if (msg != processEvent) {
-        NetPacket* pck = check_and_cast<NetPacket*>(msg);
-        bool queued = receivePacket(pck, pck->getArrivalGateId());
-
-        if (queued) {
-            if (!processEvent->isScheduled())
-                scheduleAt(simTime() + processDelay, processEvent);
-        } else {
-            EV << "Packet " << pck->getName() << " discarded.\n";
-            delete pck;
-        }
+    if (msg == emitBandwidth) {
+        emit(bandwidthSignal, bandwidth);
+        EV << "Bandwidth1: "<< bandwidth<< endl;
+        bandwidth = 0;
+        scheduleAt(simTime() + 1, emitBandwidth);
     } else {
-        NetPacket* pck = getPacketForDeparture();
-        cGate* out = gate("out");
-        cChannel* channel = out->getChannel();
-        if (channel) {
-            simtime_t finishTime = channel->getTransmissionFinishTime();
-            simtime_t delay = std::max(finishTime - simTime(), SIMTIME_ZERO);
+        if (msg != processEvent) {
+            NetPacket* pck = check_and_cast<NetPacket*>(msg);
+            bool queued = receivePacket(pck, pck->getArrivalGateId());
 
-            sendDelayed(pck, delay, out);
-        } else
-            send(pck, out);
-        if (hasPacketsAwaitingDeparture())
-            scheduleAt(simTime() + processDelay, processEvent);
+            if (queued) {
+                emit(intervalSignal, simTime().dbl() - prevTime);
+                timeRequestHistogram.collect(simTime());
+                prevTime = simTime().dbl();
+                emit(delaySignal, simTime() - pck->getArrivalTime());
+                bandwidth += pck->getByteLength();
+                EV << "Bandwidth2: "<< bandwidth<< endl;
+                acceptedCount++;
+                if (!processEvent->isScheduled()) {
+                    scheduleAt(simTime() + processDelay, processEvent);
+                }
+            } else {
+                emit(rejectedSignal, 1);
+                rejectedCount++;
+                delete pck;
+            }
+        } else {
+            NetPacket* pck = getPacketForDeparture();
+            send(pck, "out");
+
+            if (hasPacketsAwaitingDeparture())
+                scheduleAt(simTime() + processDelay, processEvent);
+        }
     }
+}
+void AbstractScheduler::finish() {
+    recordScalar("Rejected count", rejectedCount);
+    recordScalar("Accepted count", acceptedCount);
+    timeRequestHistogram.recordAs("TimeRequestHistogram");
 }
